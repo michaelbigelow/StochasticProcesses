1254232263e441239299aa9fc71bab3239e0d004
michaelbigelow <michaeljohnbigelow@gmail.com>
2020-05-14 14:27:44 -0600

    Lawler 5.6 Problem Solved

--- Lawler56.py
+++ Lawler56.py
@@ -4,7 +4,7 @@ from PolyasUrn import polyas_urn
 # part A
 bin_edges = [np.round(0.05 * i, 2) for i in range(21)]
 print("Computing proportion of trials resulting in fraction of red balls within these intervals...\n", bin_edges)
-trials = polyas_urn(num_trials=2000)[1]
+trials = polyas_urn(num_trials=2000)[0]
 hist = np.histogram(trials, bin_edges)[0]
 
 print(hist)
